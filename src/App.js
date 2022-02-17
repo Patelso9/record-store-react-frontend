619da11b37a63b93b3bec27376d78557079c0e41
Princess <sonali.patel.pkg@gmail.com>
2022-02-17 12:46:47 -0500

    adding files from frontend

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Records from './Records.js';
+
+function App() {
+  return (
+    <main className="container">
+      <Records />
+    </main>
+  );
+}
+
+export default App;
